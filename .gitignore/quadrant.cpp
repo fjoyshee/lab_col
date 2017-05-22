8dcdedc2384bc21c986c462b4fe2db9dbcc72d58
fjoyshee <fj.oyshee@gmail.com>
2017-05-22 12:40:59 +0600

    Add files via upload

---
+++ .gitignore/quadrant.cpp
@@ -0,0 +1,12 @@
+//given a coordinate (a,b),print whether it falls in the 4th quadrant
+#include <iostream>
+using namespace std;
+int main()
+{
+   double a,b;
+   cin>>a>>b;
+   if (a>0&&b<0){
+    cout<<"Yes";
+   }
+
+}
