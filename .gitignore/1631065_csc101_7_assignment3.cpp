18bde5a164b6336c491cbc15948c7d974e7d2c79
fjoyshee <fj.oyshee@gmail.com>
2017-05-22 11:49:09 +0600

    Add files via upload

---
+++ .gitignore/1631065_csc101_7_assignment3.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+
+    int a = 1;
+    int b = 2;
+
+    cout<<"Enter The First Number: ";
+    cin>>a;
+
+    cout<<endl;
+
+    cout<<"Enter The Second Number: ";
+    cin>>b;
+
+    cout<<endl;
+
+    cout << a << " + " << b << " = " << a + b <<"\n";
+    cout << a << " - " << b << " = " << a - b<<"\n";
+    cout << a << " * " << b << " = " << a * b<<"\n";
+    cout << a << " / " << b << " = " << a / b<<"\n";
+    return 0;
+}
+
+
+
+
---
+++ .gitignore/1721454_CSC101_6_Assignment5.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cout<<"\nEnter Your Desired Number :  "<<endl;
+
+    cin>>a;
+
+    if(a>0)
+    {
+        cout<<a<<"  is positive"<<endl;
+    }
+
+    else if(a<0)
+    {
+        cout<<a<<"  is negative"<<endl;
+    }
+
+    else
+    {
+        cout<<a<<"  is zero"<<endl;
+    }
+
+    return 0;
+
+}
