5e115613ade92a9441dee24db8be8ed48b63f802
fjoyshee <fj.oyshee@gmail.com>
2017-05-22 11:49:25 +0600

    Add files via upload

---
+++ .gitignore/if-else_evenodd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if (n%2==0)
+    {
+     cout<<"EVEN"<<endl;
+    }
+    else{
+
+     cout<<"ODD"<<endl;
+}
+}
