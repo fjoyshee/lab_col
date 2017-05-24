e2522f75a980360662311581daeb5e7fb4e240ab
fjoyshee <fj.oyshee@gmail.com>
2017-05-24 08:44:47 +0600

    Add files via upload

---
+++ 1722033.cpp
@@ -0,0 +1,84 @@
+//problem 1
+/*
+#include <iostream>
+using namespace std;
+int main ()
+{
+    cout<<"####### ########"<<endl;
+    cout<<"##         ##"<<endl;
+    cout<<"##         ##"<<endl;
+    cout<<"#######    ##"<<endl;
+    cout<<"##         ##"<<endl;
+    cout<<"##         ##"<<endl;
+    cout<<"#######    ##"<<endl;
+}
+*/
+
+//problem 2
+/*
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+    int x,y;
+    y=pow(x,5)+pow(x*y,.5)+pow(x*y,1.8)+abs(x-y*x-y);
+    cout<<pow(x,5)+pow(x*y,.5)+pow(x*y,1.8)+abs(x-y*x-y)<<y<<endl;
+}
+*/
+//problem 3
+/*
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+    int x1,x2,x3,x4,x5,x6,y1,y2,y3,y4,y5,y6;
+    y=(x1*y2+x2*y1)-(x3*y3+x4*y2)-(x5*y5+x6*y3);
+    cout<<(x1*y2+x2*y1)-(x3*y3+x4*y2)-(x5*y5+x6*y3<<y<<endl;
+
+}
+*/
+
+
+
+//problem 4
+/*
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+
+{
+    int a,b,c,d,e,f,dev;
+    cin>>a>>b>>c>>d>>e>>f;
+    dev=(a+b+c+d+e+f)%6;
+
+    cout<<"(a+b+c+d+e+f)%6="<<dev<<endl;
+
+}
+
+*/
+//problem 5
+/*
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+    int d1,d2,d3,d4,d5,d6,rn;
+
+cin>>d1>>d2>>d3>>d4>>d5>>d6;
+    rn=d1*100+d2*10+d3+d4*100+d5*10+d4;
+    cout<<rn<<endl;
+
+
+}
+
+
+
+
+
+
+
+
