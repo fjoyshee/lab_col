9b6a032e7b4b8e65a2e7b3574137f2457d41989a
fjoyshee <fj.oyshee@gmail.com>
2017-05-24 09:04:21 +0600

    Add files via upload

---
+++ character_if else.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{   double r1,r2;
+    char ch;
+    cin>>r1>>r2>>ch;
+    if (ch=='a'|| ch=='A')
+    {
+        cout<<r1+r2;
+    }
+
+else if (ch=='d'|| ch=='D')
+{
+    cout<<r1/r2;
+
+}
+
+}
