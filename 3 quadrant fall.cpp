26adbd003fcb8b51923b546a23b706f316274892
fjoyshee <fj.oyshee@gmail.com>
2017-05-24 09:10:44 +0600

    Add files via upload

---
+++ 3 quadrant fall.cpp
@@ -0,0 +1,14 @@
+// given a coordinate (x,y),print whelther it falls in 1st,3rd &4th quarrant
+#include <iostream>
+using namespace std;
+int main()
+{   double x,y;
+    cin>>x>>y;
+    if (!(x<0&&y>0))
+
+    {
+        cout<<"YES";
+    }
+
+
+}
